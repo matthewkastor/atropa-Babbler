867a200ec8bf1630a91d3a5eab573b13f8726389
Matthew Kastor <matthewkastor@gmail.com>
2013-09-29 01:12:59 -0400

    regenerated from source.

--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -6,6 +6,12 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="random" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-random/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="string" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-string/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <returns type="atropa"/>
                 
     };

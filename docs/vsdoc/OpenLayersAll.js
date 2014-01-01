76dab1300fa36f7479a975cb3f275dd84c8f338e
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 14:17:28 -0500

    updated docs

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -28,12 +28,6 @@
         /// <summary></summary>
         /// <field name="data" type="">Container for gobal data related to the classes and functions.</field>
         /// <field name="random" type="">Provides random strings and numbers.</field>
-        /// <field name="regex" type="">Required module, the docs for it are in the &lt;code&gt;
-        ///  atropa-regex/docs&lt;/code&gt; directory where this module 
-        ///  is located.</field>
-        /// <field name="arrays" type="">Required module, the docs for it are in the &lt;code&gt;
-        ///  atropa-arrays/docs&lt;/code&gt; directory where this module 
-        ///  is located.</field>
         /// <field name="string" type="">A few utilities for manipulating strings.</field>
         /// <returns type="atropa"/>
                 
